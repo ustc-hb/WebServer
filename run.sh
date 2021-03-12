97356afc59390d29338d86f9d1dc0e9821eebb39
hubin <45166881+ustc-hb@users.noreply.github.com>
2021-03-12 19:21:24 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # "p:l:m:c:s:n:t"
-# p:PORT  l:LOGWrite  m:LISTENTrigmode  c:CONNTrigmode  s:sql_num  n:CONNTrigmode
+# p:PORT  l:LOGWrite  m:LISTENTrigmode  c:CONNTrigmode  s:sql_num  n:thread_num
 ./webserver -p 11111 -l 0 -m 0 -c 0 -s 8 -n 8
